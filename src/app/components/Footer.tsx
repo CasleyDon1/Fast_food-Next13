8cfe5fd0e926032b32d2dc9c0345d19222da39da
CasleyDon1 <Casleynkosi@gmail.com>
2023-10-16 15:24:48 +0200

    finished the slider

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Link from "next/link";
 
 const Footer = () => {
   return (
-    <div>
-      Footer
+    <div className="h-12 md:h-24 p-4 lg:p-20 xl:p-40 flex items-center justify-between text-red-500">
+      <Link href="/" className="font-bold text-xl">Don's Spot</Link>
+      <p>All rights reserved</p>
     </div>
   );
 }
