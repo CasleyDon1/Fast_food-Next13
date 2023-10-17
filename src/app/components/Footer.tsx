6e6b951c0e45e0ac1e7123c6d2fcd37035b09ee1
CasleyDon1 <Casleynkosi@gmail.com>
2023-10-17 21:13:59 +0200

    update

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <div className="h-12 md:h-24 p-4 lg:p-20 xl:p-40 flex items-center justify-between text-red-500">
-      <Link href="/" className="font-bold text-xl">Don's Spot</Link>
+    <div className="h-12 md:h-24 p-4 lg:px-20 xl:px-40 flex items-center justify-between text-red-500">
+      <Link href="/" className="font-bold text-xl">Dons Spot</Link>
       <p>All rights reserved</p>
     </div>
   );
